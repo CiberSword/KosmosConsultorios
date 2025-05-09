2440d8eff42d9dc2401184eb5b8db2a5c5bfd0e6
Daniel JL <daniel.jarquin@docsolutions.com>
2025-05-09 13:50:35 -0600

    chore: initialize Spring project

---
+++ src/main/java/com/cibersword/kosmosconsultorios/KosmosConsultoriosApplication.java
@@ -0,0 +1,13 @@
+package com.cibersword.kosmosconsultorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmosConsultoriosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KosmosConsultoriosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cibersword/kosmosconsultorios/KosmosConsultoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibersword.kosmosconsultorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosmosConsultoriosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
